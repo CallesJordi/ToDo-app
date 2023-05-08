0bbacc8d7af81753e0321b961bb986b2c7e21960
Jordi Calles <jordicalles1989@gmail.com>
2023-05-08 19:24:44 +0200

    Change index name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("todo.json");
+const router = jsonServer.router("toDo.json");
 const port = process.env.PORT || 4000;
 
 server.use(cors());
